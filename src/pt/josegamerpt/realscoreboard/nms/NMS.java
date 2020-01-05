f3e66ba481f1a19dc3d6ad3251d8c3f8ab05facb
JoseGamerPT <josegamerpt@gmail.com>
2020-01-05 14:47:00 +0000

    small fixes

--- src/pt/josegamerpt/realscoreboard/nms/NMS.java
+++
@@ -1,9 +0,0 @@
-package pt.josegamerpt.realscoreboard.nms;
-
-import org.bukkit.entity.Player;
-
-public abstract interface NMS {
-	public abstract int pegarPing(Player paramPlayer);
-
-	public abstract void enviarActionBar(Player paramPlayer, String paramString);
-}
